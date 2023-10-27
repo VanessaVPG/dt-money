e82a1d07c7b6179809d6fcf90dae24672c9515b6
vanessavpg <vanessa.pg026@gmail.com>
2023-10-26 21:38:59 -0300

    feat: contexto para transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,29 +4,12 @@ import { useState } from "react";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 import { SearchForm } from "./components/SearchForm";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { useContext } from "react";
 
 
-interface Transaction {
-  id: number,
-  description: string,
-  type: 'income' | 'outcome',
-  category: string,
-  price: number,
-  createdAt: string
-}
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3000/transactions');
-    const data = await response.json();
-    setTransactions(data);
-
-  }
-  useEffect(() => {
-    loadTransactions();
-  }, [])
-
+  const { transactions } = useContext(TransactionsContext);
   return (
     <div>
       <Header />
